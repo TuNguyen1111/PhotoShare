7ffe9cffda2fdbf37ec432f15486f44e75e4ad73
tunv <tunv@newwave.vn>
2022-05-14 11:49:36 +0700

    add config for image

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -120,8 +120,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static')
 ]
-print(STATICFILES_DIRS)
-
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
