772ac6b897483784a55a5b72bee22c47de392234
tunv <tunv@newwave.vn>
2022-05-15 11:22:11 +0700

    get requirements files

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'photo',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
